eca4bb430f1eefef8051dee9f1520223f8b3b1c9
HotSauce97 <71828189+HotSauce97@users.noreply.github.com>
2020-10-19 19:50:44 +0300

    Update RemoteThreadInjection.cpp

--- RemoteThreadInjection.cpp
+++ RemoteThreadInjection.cpp
@@ -10,7 +10,7 @@ int main(int argc, const char* argv[])
 	if (argc < 3)
 	{
 		_tprintf(_T("Please enter <pid> <dllpath>\n"));
-		return 1;
+		return 0;
 	}
 	int pid = atoi(argv[1]);
 	HANDLE hProcess = OpenProcess(PROCESS_VM_WRITE | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION, NULL, pid);
@@ -18,7 +18,7 @@ int main(int argc, const char* argv[])
 	if (!hProcess)
 	{
 		_tprintf(_T("Cant open target process\n"));
-		return 1;
+		return 0;
 	}
 
 	void* buffer = VirtualAllocEx(hProcess, NULL, 1024, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
@@ -26,7 +26,7 @@ int main(int argc, const char* argv[])
 	{
 		_tprintf(_T("Cant allocate memory in target process\n"));
 		CloseHandle(hProcess);
-		return 1;
+		return 0;
 	}
 	WriteProcessMemory(hProcess, buffer, argv[2], strlen(argv[2]), NULL);
 	LPTHREAD_START_ROUTINE start = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(_T("kernel32")),("LoadLibraryA"));
@@ -35,8 +35,8 @@ int main(int argc, const char* argv[])
 	{
 		_tprintf(_T("Cant create thread in target process\n"));
 		CloseHandle(hProcess);
-		return 1;
+		return 0;
 	}
 	CloseHandle(hProcess);
-	return 0;
+	return 1;
 }
